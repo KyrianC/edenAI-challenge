cb34ba59f5734f855d935ee19103e445bfec4851
kyrian <ckyrian@protonmail.com>
2022-03-06 20:48:45 +0100

    fix creating receipt not working & add id fields

--- django/caisse/serializers.py
+++ django/caisse/serializers.py
@@ -6,15 +6,16 @@ from .models import Product, ReceiptItem, Receipt
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ("title", "base_price", "discount")
+        fields = ("id", "title", "base_price", "discount")
 
 
 class ReceiptItemSerializer(serializers.ModelSerializer):
     product = ProductSerializer(read_only=True)
+    product_id = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
 
     class Meta:
         model = ReceiptItem
-        fields = ("product", "quantity", "price", "scanned_at")
+        fields = ("id", "product", "product_id", "quantity", "price", "scanned_at")
 
 
 class ReceiptSerializer(serializers.ModelSerializer):
@@ -22,4 +23,14 @@ class ReceiptSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Receipt
-        fields = ("items", "total_price", "created")
+        fields = ("id", "items", "total_price", "created")
+
+    # handle writable nested serialization
+    def create(self, validated_data):
+        print(validated_data)
+        items_data = validated_data.pop("items")
+        receipt = Receipt.objects.create(**validated_data)
+        for item_data in items_data:
+            product = item_data.pop("product_id")
+            ReceiptItem.objects.create(receipt=receipt, product=product, **item_data)
+        return receipt
