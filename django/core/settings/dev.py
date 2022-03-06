69564ce6ad317be2254f74ceaf8e29e4535722fc
kyrian <ckyrian@protonmail.com>
2022-03-06 12:56:28 +0100

    init

---
+++ django/core/settings/dev.py
@@ -0,0 +1,27 @@
+import os
+from .base import *
+
+ALLOWED_HOSTS = ["*"]
+
+DEBUG = True
+
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:8000",
+    "http://localhost:3000",
+)
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("POSTGRES_ENGINE"),
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
+    }
+}
