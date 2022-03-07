3a15ef74ddb7878ae6e46644f0bacf55a03f2be0
kyrian <ckyrian@protonmail.com>
2022-03-07 10:25:10 +0100

    fix discount price error

--- vue/src/hooks/price.ts
+++ vue/src/hooks/price.ts
@@ -1,18 +1,19 @@
 type discount = 'BD' | 'PD' | null | undefined
 
-export default function(price: number, quantity = 1, discount: discount) {
+export default function(price: number, quantity = 1, discount: discount): [string, number] {
     switch (discount) {
         case 'PD': // 50% off
             price /= 2
+            break;
         case 'BD': // 2 bought 1 free
             const totalQty = 2 + 1; // 2 for bought 1 for free
             if (quantity >= totalQty) {
                 const remainder = quantity % totalQty
                 quantity = ((quantity - remainder) * 2 / totalQty) + remainder
             }
-        default:
-            const result = price * quantity
-            const formatedResult = (result / 100).toFixed(2)
-            return [formatedResult, result]
+            break;
     }
+    const result = price * quantity
+    const formatedResult = (result / 100).toFixed(2)
+    return [formatedResult, result]
 }
